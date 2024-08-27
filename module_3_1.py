fdd3034cb2e37b473482467dc20b870dde9ccc27
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-08-27 22:27:27 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,31 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    string = (len(string), string.upper(), string.lower())
+    return string
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    str_up = string.upper()
+    for i in list_to_search:
+        if str_up == i.upper():
+            return True
+            break
+    return False
+
+
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
